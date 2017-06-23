7a065c3f49b0afb5e307a3b28a04e4efbdc91001
alexanderconner <alexander.conner@accenturefederal.com>
2017-06-23 17:59:05 -0400

    Created temporary login page to verify spring functionality, added use of model object

--- src/main/java/webapp/springmvc/LoginController.java
+++ src/main/java/webapp/springmvc/LoginController.java
@@ -1,14 +1,27 @@
 package webapp.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-  @RequestMapping(value = "/login")
-  public String sayHello() {
-    return "login";
+  @RequestMapping(value = "/login2", method = RequestMethod.GET)
+  public String showLoginPage() {
+    return "login2";
   }
+
+
+  @RequestMapping(value = "/login2", method = RequestMethod.POST)
+  public String handleloginRequests(@RequestParam String username, @RequestParam String password,
+      ModelMap model) {
+    model.put("username", username);
+    model.put("password", password);
+    return "welcome2";
+  }
+
 
 }
---
+++ src/main/webapp/WEB-INF/views/welcome2.jsp
@@ -0,0 +1,15 @@
+<%@ include file="../common/header.jspf"%>
+
+<div class="container">
+
+	<p>
+		<font color="red"> ${loginError}</font>
+	</p>
+	
+	<div class="card card-container">
+	<h1>WELCOME 2</h1>
+<p>you are ${username} and password is ${password}. </p>
+
+</div>
+
+<%@ include file="../common/footer.jspf"%>
